8ffbef189ed2d0e47b74ef0282334d5a8221e94b
Yoonwon Jung <yoonwonjung12@gmail.com>
2025-12-10 23:18:23 -0800

    remove code folder from remote

--- code/website_code(php)/admin/tarit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "tar cvf $1/admin/data.tar $1/subjects/"
-tar cvf $1/admin/data/data.tar $1/subjects/
--- code/website_code(php)/checksettings.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include('settings.php');
-echo "<pre>";
-echo "Performing installation checks for emBODY tool";
-echo "current folder: ".getcwd();
-echo "folder specified in settings.php: ".$path;
-// checks that $path folder exists on the server
-if(!strcmp(getcwd(),'$path'))
-	echo "\t ERROR: it looks like the two folders do not match!";
